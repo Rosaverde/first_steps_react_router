c7da09447a3f127f2c454c0e9fd83a4e3e3b67d2
Rosaverde <rosaverde.ag@gmail.com>
2023-08-03 23:52:48 +0000

    Routes added

---
+++ src/About.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+export default function About()
+{
+    return(
+        <h5>About</h5>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import About from "./About"
 
-function App() {
+export default function App() {
 
   return (
-    <h1>Hello world</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={< App/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
